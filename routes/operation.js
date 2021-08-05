40f0fc8361db6d680713cb4e87bbbb42de9a5c74
Killian Hascoet <killianh@live.fr>
2021-08-05 21:19:10 +0200

    Fix metrics

--- routes/operation.js
+++ routes/operation.js
@@ -5,7 +5,7 @@ router.get('/metrics', function (req, res, next) {
     const end = Math.floor(Date.now() / 1000);
     const date = new Date();
     const start = Math.floor(date.getTime() / 1000) - 86400
-    const url = `http://prometheus-stack-kube-prom-prometheus.monitoring:9090/api/v1/query_range?query=sum(rate(casperholders_operations[60m])%20*%2060)%20by%20(type,%20namespace)&start=${start}&end=${end}&step=60`;
+    const url = `http://prometheus-stack-kube-prom-prometheus.monitoring:9090/api/v1/query_range?query=sum(increase(casperholders_operations[1h]))%20by%20(type,%20namespace)&start=${start}&end=${end}&step=60`;
 
     fetch(url).then((response) => {
         response.json().then((data) => {
