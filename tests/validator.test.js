cbc22bd84ee011434a3c39c2eb4e5ddc348c479a
Killian Hascoet <killianh@live.fr>
2021-11-29 02:07:03 +0100

    removed console.log

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -14,7 +14,6 @@ describe('Deploy endpoint', () => {
   it('should return 200 to retrieve validator infos', async () => {
     const res = await request(app)
       .get('/validators/accountinfos');
-    console.log(res.body.filter((validator) => validator.publicKey === "0124bfdae2ed128fa5e4057bc398e4933329570e47240e57fc92f5611a6178eba5"));
     expect(res.statusCode).toEqual(200);
   });
 });
