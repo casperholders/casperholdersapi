d7d318e670a67066982e8a7eb4e8b2fe10b53ca3
Killian Hascoet <killianh@live.fr>
2023-08-10 18:06:23 +0200

    Modify conf

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-let express = require("express");
-let cors = require("cors");
-let path = require("path");
-let cookieParser = require("cookie-parser");
-let bodyParser = require("body-parser");
-let logger = require("morgan");
-let validatorsRouter = require("./routes/validator").router;
-let apyRouter = require("./routes/apy").router;
-let deploysRouter = require("./routes/deploys");
+let express = require('express');
+let cors = require('cors');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let logger = require('morgan');
+let validatorsRouter = require('./routes/validator').router;
+let apyRouter = require('./routes/apy').router;
+let deploysRouter = require('./routes/deploys');
+
+console.log(process.env.CASPER_RPC_URL);
 
 let app = express();
 
 app.use(cors({
-    origin: process.env.ORIGIN.split(","),
-    optionsSuccessStatus: 200,
+  origin: process.env.ORIGIN.split(','),
+  optionsSuccessStatus: 200,
 }));
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use("/validators", validatorsRouter);
-app.use("/apy", apyRouter);
-app.use("/deploys", deploysRouter);
+app.use('/validators', validatorsRouter);
+app.use('/apy', apyRouter);
+app.use('/deploys', deploysRouter);
 
 
 module.exports = app;
